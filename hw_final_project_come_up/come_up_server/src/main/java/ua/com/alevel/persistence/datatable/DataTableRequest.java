e0b5e9c3f71ecc7bf787017dc1996fd183c0b496
IzzatAliev <alibax1515@gmail.com>
2022-01-10 01:11:31 +0200

    add persistence done

--- hw_final_project_come_up/come_up_server/src/main/java/ua/com/alevel/persistence/datatable/DataTableRequest.java
+++ hw_final_project_come_up/come_up_server/src/main/java/ua/com/alevel/persistence/datatable/DataTableRequest.java
@@ -1,8 +1,13 @@
 package ua.com.alevel.persistence.datatable;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Collections;
 import java.util.Map;
 
+@Getter
+@Setter
 public class DataTableRequest {
 
     private int page;
@@ -13,45 +18,5 @@ public class DataTableRequest {
 
     public DataTableRequest() {
         requestParamMap = Collections.emptyMap();
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public String getOrder() {
-        return order;
-    }
-
-    public void setOrder(String order) {
-        this.order = order;
-    }
-
-    public String getSort() {
-        return sort;
-    }
-
-    public void setSort(String sort) {
-        this.sort = sort;
-    }
-
-    public Map<String, String[]> getRequestParamMap() {
-        return requestParamMap;
-    }
-
-    public void setRequestParamMap(Map<String, String[]> requestParamMap) {
-        this.requestParamMap = requestParamMap;
     }
 }
--- hw_final_project_come_up/come_up_server/src/main/java/ua/com/alevel/persistence/datatable/DataTableResponse.java
+++ hw_final_project_come_up/come_up_server/src/main/java/ua/com/alevel/persistence/datatable/DataTableResponse.java
@@ -1,11 +1,15 @@
 package ua.com.alevel.persistence.datatable;
 
+import lombok.Getter;
+import lombok.Setter;
 import ua.com.alevel.persistence.entity.BaseEntity;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 public class DataTableResponse<E extends BaseEntity> {
 
     private int currentPage;
@@ -21,69 +25,5 @@ public class DataTableResponse<E extends BaseEntity> {
         this.pageSize = 10;
         this.items = Collections.emptyList();
         this.otherParamMap = Collections.emptyMap();
-    }
-
-    public int getCurrentPage() {
-        return currentPage;
-    }
-
-    public void setCurrentPage(int currentPage) {
-        this.currentPage = currentPage;
-    }
-
-    public int getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    public int getTotalPages() {
-        return totalPages;
-    }
-
-    public void setTotalPages(int totalPages) {
-        this.totalPages = totalPages;
-    }
-
-    public long getItemsSize() {
-        return itemsSize;
-    }
-
-    public void setItemsSize(long itemsSize) {
-        this.itemsSize = itemsSize;
-    }
-
-    public String getSort() {
-        return sort;
-    }
-
-    public void setSort(String sort) {
-        this.sort = sort;
-    }
-
-    public String getOrder() {
-        return order;
-    }
-
-    public void setOrder(String order) {
-        this.order = order;
-    }
-
-    public List<E> getItems() {
-        return items;
-    }
-
-    public void setItems(List<E> items) {
-        this.items = items;
-    }
-
-    public Map<Object, Object> getOtherParamMap() {
-        return otherParamMap;
-    }
-
-    public void setOtherParamMap(Map<Object, Object> otherParamMap) {
-        this.otherParamMap = otherParamMap;
     }
 }
